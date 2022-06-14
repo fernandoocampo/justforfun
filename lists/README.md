45336aff643252fcb0926ea1041ec040b8a4b108
Fernando Ocampo <fdocampo@gmail.com>
2022-06-15 00:41:44 +0200

    add merge multiple lists

--- lists/README.md
+++ lists/README.md
@@ -47,3 +47,62 @@ go test -run none -bench MergetTwoLists -benchtime=5s -benchmem -cpuprofile=cpu.
 go tool pprof lists.test cpu.out
 go tool pprof -alloc_space lists.test mem.out
 ```
+
+
+# Merge k Sorted Lists
+
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 104
+0 <= lists[i].length <= 500
+-104 <= lists[i][j] <= 104
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 10^4.
+
+## Testing and Benchmarking
+
+* test
+
+```sh
+go test -timeout 3s -count 1 -run ^TestMergeKLists$ github.com/fernandoocampo/justforfun/lists
+```
+
+* bench
+
+```sh
+rm cpu.out mem.out trace.out lists.test
+
+go test -run none -bench MergetKLists -benchtime=5s -benchmem -cpuprofile=cpu.out -memprofile=mem.out -trace=trace.out github.com/fernandoocampo/justforfun/lists
+
+go tool pprof lists.test cpu.out
+go tool pprof -alloc_space lists.test mem.out
+```
--- lists/merge.go
+++ lists/merge.go
@@ -1,78 +1,5 @@
 package lists
 
-import (
-	"strconv"
-	"strings"
-)
-
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
-func NewListNode(values []int) *ListNode {
-	var head *ListNode
-	if len(values) == 0 {
-		return head
-	}
-	head = &ListNode{}
-	head.Val = values[0]
-	follower := head
-	for i := 1; i < len(values); i++ {
-		newnode := ListNode{
-			Val: values[i],
-		}
-		follower.Next = &newnode
-		follower = &newnode
-	}
-	return head
-}
-
-func (l *ListNode) Value() int {
-	if l != nil {
-		return l.Val
-	}
-	return 0
-}
-
-func (l *ListNode) NextNode() *ListNode {
-	if l != nil {
-		return l.Next
-	}
-	return nil
-}
-
-func (l *ListNode) Len() int {
-	if l == nil {
-		return 0
-	}
-	count := 0
-	for i := l; ; i = i.Next {
-		if i == nil {
-			break
-		}
-		count++
-	}
-	return count
-}
-
-func (l *ListNode) String() string {
-	if l == nil {
-		return ""
-	}
-	b := strings.Builder{}
-	for i := l; ; i = i.NextNode() {
-		if i == nil {
-			break
-		}
-		b.WriteString(strconv.Itoa(i.Value()))
-		if i.NextNode() != nil {
-			b.WriteRune('-')
-		}
-	}
-	return b.String()
-}
-
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -135,4 +62,61 @@ func merge(list1, list2, new *ListNode) {
 	new = new.Next
 	new.Next = &ListNode{Val: list2.Val}
 	merge(list1.Next, list2.Next, new.Next)
+}
+
+func MergetKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+	if len(lists) == 1 {
+		return lists[0]
+	}
+	return mergeLists(lists, nil)
+}
+
+func mergeLists(lists []*ListNode, out *ListNode) *ListNode {
+	if len(lists) == 0 {
+		return out
+	}
+
+	var l2 []*ListNode
+	for _, node := range lists {
+		if node == nil {
+			continue
+		}
+		out = feedNode(node.Val, out, out)
+		l2 = append(l2, node.Next)
+	}
+
+	return mergeLists(l2, out)
+}
+
+func feedNode(val int, head, former *ListNode) *ListNode {
+	if former == nil {
+		return &ListNode{Val: val}
+	}
+	if former != nil && val < former.Val {
+		newNode := &ListNode{
+			Val:  val,
+			Next: former,
+		}
+		if head == former {
+			former = newNode
+			return former
+		}
+		return head
+	}
+	if former != nil && former.Next == nil {
+		former.Next = &ListNode{Val: val}
+		return head
+	}
+	if val < former.Next.Val {
+		newNode := &ListNode{
+			Val:  val,
+			Next: former.Next,
+		}
+		former.Next = newNode
+		return head
+	}
+	return feedNode(val, head, former.Next)
 }
